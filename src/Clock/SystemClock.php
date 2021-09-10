c4391367b5ae117bfea84c00291f574e9f7cd7c2
zero <teclaizerai@gmail.com>
2021-09-10 10:01:47 +0200

    Fix cs.

--- src/Clock/SystemClock.php
+++ src/Clock/SystemClock.php
@@ -13,7 +13,7 @@ class SystemClock implements Clock
     {
         $this->timeZone = $timeZone ?? new DateTimeZone('UTC');
     }
-    
+
     public function time(): DateTimeImmutable
     {
         return new DateTimeImmutable('now', $this->timeZone);
